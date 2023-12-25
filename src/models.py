b0e96924d6e43ef2bc5206c0263123af98ffb0a1
AddChew <addison_chew@outlook.com>
2023-12-25 18:11:53 +0800

    update models.py

--- src/models.py
+++ src/models.py
@@ -6,8 +6,14 @@ class Users(models.Model):
     username = fields.CharField(max_length = 20, unique = True)
     hashed_password = fields.CharField(max_length = 255)
 
+    class PydanticMeta:
+        exclude = ["id", "hashed_password"]
+
 
 class Projects(models.Model):
     id = fields.IntField(pk = True)
     name = fields.CharField(max_length = 50)
     owner = fields.ForeignKeyField(model_name = "models.Users", related_name = "projects")
+
+    class PydanticMeta:
+        exclude = ["id"]
