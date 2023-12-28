d83bf2645b8ef02fd19300b0b5bf50f60c7fa7e1
AddChew <addison_chew@outlook.com>
2023-12-29 01:22:48 +0800

    add conftest fixture

--- conftest.py
+++ conftest.py
@@ -1,3 +1,17 @@
 import os
+import pytest
+
 
 os.environ["db_url"] = "sqlite://:memory:"
+os.environ["root_module"] = "tests"
+
+
+@pytest.fixture
+def monkeypatch_users_dir(monkeypatch):
+    """
+    Monkeypatch USERS_DIR.
+    """
+    monkeypatch.setattr(
+        "src.projects.USERS_DIR",
+        os.path.join(os.path.abspath(os.path.dirname(os.path.abspath(__file__))), "tests", "users")
+    )
