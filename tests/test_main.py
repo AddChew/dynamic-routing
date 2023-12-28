aadd2f01eefa461e9a04e0f45c6956db7272b286
AddChew <addison_chew@outlook.com>
2023-12-29 01:44:26 +0800

    add create project tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,17 @@
+import os
+import shutil
+
 from fastapi.testclient import TestClient
 from src.main import app
+
+
+def login_user(
+        client: TestClient, credentials: dict = {"username": "test_user", "password": "test_password"}, 
+        register_route: str = "/auth/register", login_route: str = "/auth/login"
+        ) -> str:
+    client.post(register_route, data = credentials)
+    response = client.post(login_route, data = credentials)
+    return response.json()["access_token"]
 
 
 class TestAuthAPI:
@@ -96,3 +108,79 @@ class TestAuthAPI:
             json_response = response.json()
             assert json_response["token_type"] == "bearer"
             assert "access_token" in json_response
+
+
+class TestProjectsAPI:
+
+    def setup_class(self):
+        tests_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+        scripts_dir = os.path.join(tests_dir, "scripts")
+
+        self.users_dir = os.path.join(tests_dir, "users")
+        self.project_script_1 = os.path.join(scripts_dir, "demo_app1.py")
+
+    def teardown_method(self):
+        for item in os.listdir(self.users_dir):
+            path = os.path.join(self.users_dir, item)
+            if os.path.isdir(path):
+                shutil.rmtree(path, ignore_errors = True)
+
+    def test_unauth_create_project(self):
+        with TestClient(app) as client, open(self.project_script_1, "rb") as f:
+            response = client.post(
+                "/test_user", 
+                data = {"project_name": "test_project"},
+                files = {"project_script": f},
+            )
+            assert response.status_code == 401
+            assert response.json() == {
+                "detail": "Not authenticated"
+            }
+
+    def test_create_new_project(self, monkeypatch_users_dir):
+        with TestClient(app) as client, open(self.project_script_1, "rb") as f:
+            access_token = login_user(client)
+            response = client.post(
+                "/test_user",
+                headers = {"Authorization": f"Bearer {access_token}"},
+                data = {"project_name": "test_project"},
+                files = {"project_script": f},
+            )
+            assert response.status_code == 200
+            assert response.json() == {
+                "name": "test_project",
+                "owner": {
+                    "username": "test_user"
+                }
+            }
+
+            user_dir = os.path.join(self.users_dir, "test_user")
+            project_dir = os.path.join(user_dir, "test_project")
+            project_script = os.path.join(project_dir, "app.py")
+
+            assert os.path.isdir(user_dir)
+            assert os.path.isdir(project_dir)
+            assert os.path.isfile(project_script)
+
+            response = client.get("/test_user/test_project/")
+            assert response.status_code == 200
+            assert response.json() == "index!"
+
+    def test_create_existing_project(self, monkeypatch_users_dir):
+        with TestClient(app) as client, open(self.project_script_1, "rb") as f:
+            access_token = login_user(client)
+            client.post(
+                "/test_user",
+                headers = {"Authorization": f"Bearer {access_token}"},
+                data = {"project_name": "test_project"},
+                files = {"project_script": f},
+            )
+
+            response = client.post(
+                "/test_user",
+                headers = {"Authorization": f"Bearer {access_token}"},
+                data = {"project_name": "test_project"},
+                files = {"project_script": f},
+            )
+            assert response.status_code == 409
+            assert response.json() == {"detail": "Project already exists."}
