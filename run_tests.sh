183efec86aca30fe37bfd1b3eae2f36df4312515
AddChew <addison_chew@outlook.com>
2023-12-31 00:57:06 +0800

    add test sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+test_env=dynamic-routing-tests
+
+if ! conda env list | grep $test_env >/dev/null 2>&1; then
+    conda create -n dynamic-routing-tests python=3.10 -y
+fi;
+
+source activate $test_env
+pip install -r deploy/src/requirements.txt
+pip install pylint pytest coverage httpx
+
+pylint $(git ls-files '*.py')
+coverage run -m pytest -v
+coverage report -m
